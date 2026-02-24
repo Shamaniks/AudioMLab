API structure, fixed normalization

main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import librosa
-from model import Model
+
+from model import Sequential
+from layers import Dense, Relu, Sigmoid
 
 def generate_synthetic_data():
     sr = 16000 
@@ -19,15 +21,28 @@ def generate_synthetic_data():
 
 x_yes, x_noise = generate_synthetic_data()
 
-brain = Model(size=13)
+all_data = np.concatenate([x_yes, x_noise], axis=0)
+mean = np.mean(all_data, axis=0)
+std = np.std(all_data, axis=0) + 1e-8
+
+x_yes = (x_yes - mean) / std
+x_noise = (x_noise - mean) / std
+
+
+model = Sequential([
+    Dense(13, 32),
+    Relu,
+    Dense(32, 1),
+    Sigmoid,
+])
 
 print("Starting smoke test...")
 for i in range(101):
-    p_yes = brain.forward(x_yes)
-    brain.backward(1.0, lr=0.1)
+    p_yes = model.forward(x_yes)
+    model.backward(p_yes - 1.0, lr=0.1)
     
-    p_noise = brain.forward(x_noise)
-    brain.backward(0.0, lr=0.1)
+    p_noise = model.forward(x_noise)
+    model.backward(p_noise - 0.0, lr=0.1)
     
     if i % 20 == 0:
         print(f"Epoch {i:3}: Command prob={p_yes[0][0]:.4f} | Noise prob={p_noise[0][0]:.4f}")

model.py
@@ -1,21 +1,13 @@
-import numpy as np
+class Sequential:
+    def __init__(self, layers):
+        self.layers = layers
 
-class Model:
-    def __init__(self, size=13):
-        self.W = np.random.randn(size, 1) * np.sqrt(1 / size)
-        self.b = 0.0
+    def forward(self, input_data):
+        for layer in self.layers:
+            input_data = layer.forward(input_data)
+        return input_data
 
-    def forward(self, x):
-        self.x = x
-        self.z = np.dot(x, self.W) + self.b
-        self.a = 1 / (1 + np.exp(-self.z)) # Sigmoid
-        return self.a
-
-    def backward(self, y_true, lr=0.1):
-        da = self.a - y_true
-        
-        dW = np.dot(self.x.T, da)
-        db = np.sum(da)
-        
-        self.W -= lr * dW
-        self.b -= lr * db
+    def backward(self, output_gradient, lr):
+        for layer in reversed(self.layers):
+            output_gradient = layer.backward(output_gradient, lr)
+        return output_gradient